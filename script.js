9528c1b9491c3bc1b5f835a150f3ad6bebd730f2
Troy Johnson <tnj8510@gmail.com>
2021-07-01 22:27:29 -0700

    added comments

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ var curDate = $('#currentDay')
 
 $('button').on("click", function () {
     console.log('clicked')
+    //save value at .description to note
     note = $(this).siblings('.description').val();
+    //save id of block we access with click
     slot = $(this).parent().attr('id');
-    console.log('note', note)
-    console.log('slot', slot)
     // save in localStorage
     localStorage.setItem(slot, note);
 });
@@ -13,22 +13,19 @@ $('button').on("click", function () {
 function getSchedule() {
 
     for (var i = 9; i < 18; i++) {
-        var temp = "";
+        
         var str = i.toString();
+        //look for id, store item in child
         $('#' + str).children().eq(1).val(localStorage.getItem(str));
-
     }
-
 }
-
-
 function setSchedule() {
-
+    //display date at id current day
     document.querySelector("#currentDay").textContent = moment().format("dddd, MMMM Do")
 
     var curTime24 = Number(moment().format("k"))
 
-    var curTime24 = 14;
+    //color schedule depending on current time
     for (var i = 9; i < 18; i++) {
         if (curTime24 > i)
             $(".description").eq(i - 9).addClass("past");
@@ -37,8 +34,6 @@ function setSchedule() {
         else if (curTime24 < i)
             $(".description").eq(i - 9).addClass("future");
     }
-
-
 }
 
 setSchedule();
